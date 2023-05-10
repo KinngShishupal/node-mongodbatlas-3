52e4c8861d7fc46c27d972fe6ffdf1e40ee72924
Shishupal singh <Shishupal.singh@satin.local>
2023-05-10 15:44:30 +0530

    created user with modified request

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,13 @@ const User = require('../models/userModel');
 
 const signup = async (req, res, next) => {
   try {
-    const newUSer = await User.create(req.body);
+    // const newUSer = await User.create(req.body);
+    const newUSer = await User.create({
+      name:req.body.name,
+      email:req.body.email,
+      password:req.body.password,
+      passwordConfirm:req.body.passwordConfirm
+    });
     res.status(201).json({
       status: 'success',
       data: {
