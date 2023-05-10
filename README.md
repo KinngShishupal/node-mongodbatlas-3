f9e2f3b4184c9dabc298e4875ce070fabf9fedd6
Shishupal singh <Shishupal.singh@satin.local>
2023-05-10 16:02:05 +0530

    added jwt

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Modelling Users
 2. Creating route for adding user
 3. Managing Password (hashing/encrypting passwords)
+4. jwt token creation
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require('../models/userModel');
+var jwt = require('jsonwebtoken');
+
 
 const signup = async (req, res, next) => {
   try {
@@ -10,8 +12,12 @@ const signup = async (req, res, next) => {
       password:req.body.password,
       passwordConfirm:req.body.passwordConfirm
     });
+
+    // const token = jwt.sign(object for all the data we want to store insidetoken, secret String, options)
+    const token = jwt.sign({id:newUSer._id},process.env.JWT_SECRET, {expiresIn:process.env.JWT_EXPIRES_IN})
     res.status(201).json({
       status: 'success',
+      token,
       data: {
         user: newUSer,
       },
